1362e6c1aa8d84ea802cd2e0b97da6abb5a6f89a
Julien Bertram <bertramjulien@gmail.com>
2022-11-21 17:10:39 +0100

    controller + views done

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,29 @@
 class AppointmentsController < ApplicationController
+  before_action :set_user, :set_partner
+
+  def index
+    @appointments = @user.appointments
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(params_appointment)
+  end
+
+  private
+
+  def params_appointment
+    params.require(:appointment).permit(:schedule_date, :comment, :user_id, :partner_id)
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_partner
+    @partner = Partner.find(params[partner_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :partners
-  resources :appointments, only: [:new, :create]
+  resources :partners do
+    resources :appointments, only: [:new, :create]
+  end
+    resources :appointments, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
