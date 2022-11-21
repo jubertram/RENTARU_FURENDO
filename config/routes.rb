214a2ede5895847e138f667f2d7b61894f2f057c
nunucuit <rises-426@hotmail.fr>
2022-11-21 16:01:55 +0100

    troisième save

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :partners
+  resources :appointments, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
