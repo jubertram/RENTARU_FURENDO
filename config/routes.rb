daf7e361a98732ef4b0fb96ab9b7e556de000429
Julien Bertram <bertramjulien@gmail.com>
2022-11-22 12:55:23 +0100

    delete appointments done

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :partners do
-    resources :appointments, only: [:new, :create]
+    resources :appointments, only: %i[new create]
   end
-  resources :appointments, only: [:index, :destroy]
+  resources :appointments, only: %i[index destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
